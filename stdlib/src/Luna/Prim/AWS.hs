0b3b1c1bee03c91d6312883e1f5c160497c4fe15
piotrMocz <piotr.moczurad@gmail.com>
2018-08-12 10:38:53 +0200

    Invoking AWS Lambda functions

--- stdlib/src/Luna/Prim/AWS.hs
+++ stdlib/src/Luna/Prim/AWS.hs
@@ -4,8 +4,10 @@ module Luna.Prim.AWS where
 
 import Prologue
 
+import Data.ByteString  (ByteString)
 import Data.Map         (Map)
-import Luna.Std.Builder (makeFunctionIO)
+import Data.Text        (Text)
+import Luna.Std.Builder (LTp (..), makeFunctionIO)
 
 import qualified Data.Map                    as Map
 import qualified Luna.IR                     as IR
@@ -14,18 +16,48 @@ import qualified Luna.Runtime                as Luna
 import qualified Luna.Std.Builder            as Builder
 import qualified Network.AWS                 as AWS
 import qualified Network.AWS.Lambda          as Lambda
+import qualified OCI.Data.Name               as Name
 
+
+type AWSModule = "Std.AWS"
+
+awsModule :: Name.Qualified
+awsModule = Name.qualFromSymbol @AWSModule
 
 exports :: forall graph m. Builder.StdBuilder graph m => m (Map IR.Name Def.Def)
 exports = do
-    let listFunsVal :: IO ()
-        listFunsVal = do
-          env  <- AWS.newEnv AWS.Discover
+    let envT        = LCons awsModule "AWSEnv"               []
+        invokeRespT = LCons awsModule "LambdaInvokeResponse" []
+
+    let listFunsVal :: AWS.Env -> IO ()
+        listFunsVal env = do
           funs <- AWS.runResourceT $ AWS.runAWS env
                                    $ AWS.send
                                    $ Lambda.listFunctions
           print funs
-    primListFuns <- makeFunctionIO @graph (flip Luna.toValue listFunsVal) [] Builder.noneLT
+    primListFuns <- makeFunctionIO @graph (flip Luna.toValue listFunsVal)
+                                          [envT] Builder.noneLT
 
-    pure $ Map.fromList [ ("primListFuns", primListFuns) ]
+    let newEnvVal :: IO AWS.Env
+        newEnvVal = AWS.newEnv AWS.Discover
+    primAWSNewEnv <- makeFunctionIO @graph (flip Luna.toValue newEnvVal)
+                                           [] envT
 
+    let invokeVal :: AWS.Env -> Text -> ByteString -> IO Lambda.InvokeResponse
+        invokeVal env fname payload =
+            let invocation = Lambda.invoke fname payload
+            in AWS.runResourceT . AWS.runAWS env . AWS.send $ invocation
+        invokeArgsT = [envT, Builder.textLT, Builder.binaryLT]
+    primAWSInvoke <- makeFunctionIO @graph (flip Luna.toValue invokeVal)
+                                           invokeArgsT invokeRespT
+
+    pure $ Map.fromList [ ("primAWSListFuns", primListFuns)
+                        , ("primAWSNewEnv",   primAWSNewEnv)
+                        , ("primAWSInvoke",   primAWSInvoke)
+                        ]
+
+
+type instance Luna.RuntimeRepOf AWS.Env =
+    Luna.AsNative ('Luna.ClassRep AWSModule "AWSEnv")
+type instance Luna.RuntimeRepOf Lambda.InvokeResponse =
+    Luna.AsNative ('Luna.ClassRep AWSModule "LambdaInvokeResponse")
