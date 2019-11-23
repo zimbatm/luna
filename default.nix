a311195a509dd8d2e63268e4fbcd1a0fdde8d7d8
zimbatm <zimbatm@zimbatm.com>
2019-11-23 23:43:02 +0100

    WIP

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 let
-  sources = import ./nix/sources.nix;
+  #haskell-nix = sources."haskell.nix";
+  haskell-nix = /home/zimbatm/go/src/github.com/input-output-hk/haskell.nix;
 
-  pkgs = import sources.nixpkgs (import sources."haskell.nix");
+  pkgs = import "${haskell-nix}/nixpkgs" (import haskell-nix);
 in
   pkgs.haskell-nix.stackProject {
     src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
